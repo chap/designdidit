e44ed1e7a02e47966814200b6313558310a7cc40
Chap Ambrose <chapambrose@gmail.com>
2010-10-20 13:49:21 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Designdidit::Application.config.secret_token = 'eb839e8884de42dffa77ab07a274393b3bb5b4d6886bd9fdfd3e4c02e69592ad4c4847ac78d78f63fdb977f6b38b904bb5f18ee7bf5bd7006c95d80ca43ce99f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Designdidit::Application.config.session_store :cookie_store, :key => '_designdidit_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# Designdidit::Application.config.session_store :active_record_store
