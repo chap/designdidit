1de7b0b44ae32422ef8a00dca44fb058f6f5e218
Chap Ambrose <chapambrose@gmail.com>
2010-10-20 18:31:08 -0500

    add some seed

--- app/helpers/designs_helper.rb
+++ app/helpers/designs_helper.rb
@@ -5,7 +5,11 @@ module DesignsHelper
     result_text = []
     text.each do |t|
       t = content_tag(:span, h(t), :class => 'highlight') if t.downcase == 'design'
+      t = content_tag(:span, h(t), :class => 'highlight') if t.downcase.include?('%')
+      t = content_tag(:span, h(t), :class => 'highlight') if t.downcase.include?('$')
       t = content_tag(:span, h(t), :class => 'highlight') if t.to_f.to_s == t
+      t = content_tag(:span, h(t), :class => 'highlight') if t.to_i.to_s == t
+      t = content_tag(:span, h(t), :class => 'highlight') if t.gsub(',','').to_i.to_s == t.gsub(',','')
       result_text << t
     end
     result_text.join(' ')
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,20 @@
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
 Design.create([
-  {:description => "Design sold 14.1 million iPhones.", :source_url => "http://www.apple.com/pr/library/2010/10/18results.html", :tag_list => "apple, iphone"}
+  {:description => "Design sold 14.1 million iPhones.", :source_url => "http://www.apple.com/pr/library/2010/10/18results.html", :tag_list => "apple, electronics"},
+  {:description => "Design increased ad impressions by 95%.", :source_url => "http://www.empathylab.com/", :tag_list => "ads, website"},
+  {:description => "Design gave 5,000 aboriginal children in Canada laptops.", :source_url => "http://www.firstperspective.ca/index.php/news/656-laptop", :tag_list => "social, electronics"},
+  {:description => "Design increased conversion by 40%.", :source_url => "http://www.lukew.com/ff/entry.asp?1007", :tag_list => "website, conversion"},
+  {:description => "Design sold 1,581,887 cars.", :source_url => "http://en.wikipedia.org/wiki/Mini#Popularity", :tag_list => "cars"},
+  {:description => "Design employed 300 people.", :source_url => "http://www.tour-eiffel.fr/teiffel/uk/documentation/dossiers/page/construction.html", :tag_list => "employment, architecture"},
+  {:description => "Design reduced the cooking time of rice to 1 minute.", :source_url => "http://www.minuterice.com/", :tag_list => "food"},
+  {:description => "Design allowed 6 men to spend 50 hours playing video games.", :source_url => "http://www.tomsguide.com/us/guinness-world-record-red-dead-redemption-gaming,news-7526.html", :tag_list => "game"},
+  {:description => "Design brought clean water to 1,277,430 people.", :source_url => "http://www.charitywater.org/", :tag_list => 'social'},
+  {:description => "Design sold $50 million in computers a day.", :source_url => "http://www.frogdesign.com/case-study/dell.com.html", :tag_list => "dell, electronics"}
 ])
+
+# hippo roller
+# mini cooper
+# herman miller chair
+# target ?
+# rakes at home depot
